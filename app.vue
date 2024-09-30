38d61e30290e9ba4f209dd79a6da91e18b715fdd
Mark Liang <mark@vdr.tw>
2024-09-30 16:51:57 +0800

    feat: upgrade to newest nuxt3

--- app.vue
+++ app.vue
@@ -6,15 +6,15 @@
 </template>
 
 <script lang="ts" setup>
-import structuredData from '@/assets/LD.json';
-import { useHead } from '#imports';
+import structuredData from "@/assets/LD.json";
+import { useHead } from "#imports";
 const config = useRuntimeConfig();
 const BRAND = config.public.BRAND_NAME;
 
 useHead({
-  script: [{ type: 'application/ld+json', innerHTML: structuredData }],
+  script: [{ type: "application/ld+json", innerHTML: structuredData }],
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} - ${BRAND}` : BRAND;
-  }
+  },
 });
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ const BASE_URL = "/";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
