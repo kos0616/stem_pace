b8579291a88de8b251b1bbf863ff0203feefb8c3
Mark Liang <mark@vdr.tw>
2025-05-15 15:39:26 +0800

    feat: 新增文字簡化，調整csv檔名，加入使用教學，修正圖片上傳區若先打字則無法上圖的bug

--- components/download/tutorial.vue
+++ components/download/tutorial.vue
@@ -1,95 +1,109 @@
 <template>
-  <div class="md:flex gap-x-10 items-center justify-between">
-    <article class="w-full sm:w-2/3 lg-w-1/2 mb-5 md:mb-0 mx-auto">
-      <h2 class="text-2xl title">我該如何上手？</h2>
-      <ol class="list-decimal pl-5">
-        <li>
-          前往
-          <b>CT Yeh 公路車基地</b>，選擇路線配速。
+  <div>
+    <div class="md:flex gap-x-10 items-center justify-between">
+      <article class="w-full sm:w-2/3 lg-w-1/2 mb-5 md:mb-0 mx-auto">
+        <h2 class="text-2xl title">我該如何上手？</h2>
+        <ol class="list-decimal pl-5">
+          <li>
+            前往
+            <b>CT Yeh 公路車基地</b>，選擇路線配速。
+            <a
+              href="https://www.ctyeh.com/routelist"
+              title="前往連結 CT Yeh 公路車基地(開新分頁)"
+              target="_blank"
+              class="underline text-primary"
+            >
+              <i class="fas fa-link fa-fw"></i>
+              連結在此
+            </a>
+          </li>
+          <li>
+            下載小抄，檔案格式為 *.csv，以西進武嶺為例，則檔名為
+            <b>西進武嶺_配速表.csv</b>。
+          </li>
+          <li>
+            <button
+              class="bg-green-600 font-bold text-white px-1 rounded"
+              @click="onUploadClicked"
+            >
+              匯入 配速表.csv
+            </button>
+            匯入成功可在底下預覽與編輯。
+          </li>
+          <li>
+            <b class="bg-blue-500 text-white px-1 rounded">列印小抄</b>
+            取得您獨一無二的配速表，可自行列印或是上傳至超商雲端列印。
+          </li>
+          <li>
+            列印後剪下，經護貝或用膠帶黏貼正反兩面即可防水。
+
+            <NuxtLink
+              class="underline text-primary"
+              to="/intro#usuage"
+              target="_blank"
+            >
+              小抄怎麼用?
+            </NuxtLink>
+          </li>
+        </ol>
+      </article>
+
+      <div
+        class="w-full sm:w-2/3 lg-w-1/2 flex gap-x-4 justify-center text-center items-start mx-auto"
+      >
+        <div class="mb-5 md:mb-0">
+          <uploader @handleUpload="handleUpload" :isUpload="isUpload" />
           <a
             href="https://www.ctyeh.com/routelist"
-            title="前往連結 CT Yeh 公路車基地(開新分頁)"
             target="_blank"
-            class="underline text-primary"
+            class="underline text-primary my-8 lg:mt-3 lg:mb-0 block"
+            title="前往 CT Yeh 公路車基地"
           >
-            <i class="fas fa-link fa-fw"></i>
-            連結在此
+            取得配速表
           </a>
-        </li>
-        <li>
-          下載小抄，檔案格式為 *.csv，以西進武嶺為例，則檔名為
-          <b>西進武嶺_配速表.csv</b>。
-        </li>
-        <li>
+        </div>
+        <div class="text-center">
           <button
-            class="bg-green-600 font-bold text-white px-1 rounded"
-            @click="onUploadClicked"
+            :disabled="!isUpload"
+            onclick="window.print()"
+            class="btn-print"
+            title="列印小抄"
+            type="button"
           >
-            匯入 配速表.csv
+            <i class="fas fa-print md:text-2xl" aria-hidden="true" />
+            列印小抄
           </button>
-          匯入成功可在底下預覽與編輯。
-        </li>
-        <li>
-          <b class="bg-blue-500 text-white px-1 rounded">列印小抄</b>
-          取得您獨一無二的配速表，可自行列印或是上傳至超商雲端列印。
-        </li>
-        <li>
-          列印後剪下，經護貝或用膠帶黏貼正反兩面即可防水。
-
-          <NuxtLink
-            class="underline text-primary"
-            to="/intro#usuage"
+          <a
+            href="https://print.ibon.com.tw/ibonprinter"
             target="_blank"
+            class="underline text-primary my-8 lg:mt-3 lg:mb-0 block"
+            title="使用 7-11 雲端列印"
           >
-            小抄怎麼用?
-          </NuxtLink>
-        </li>
-      </ol>
-    </article>
-
-    <div
-      class="w-full sm:w-2/3 lg-w-1/2 flex gap-x-4 justify-center text-center items-start mx-auto"
-    >
-      <div class="mb-5 md:mb-0">
-        <uploader @handleUpload="handleUpload" :isUpload="isUpload" />
-        <a
-          href="https://www.ctyeh.com/routelist"
-          target="_blank"
-          class="underline text-primary my-8 lg:mt-3 lg:mb-0 block"
-          title="前往 CT Yeh 公路車基地"
-        >
-          取得配速表
-        </a>
-      </div>
-      <div class="text-center">
-        <button
-          :disabled="!isUpload"
-          onclick="window.print()"
-          class="btn-print"
-          title="列印小抄"
-          type="button"
-        >
-          <i class="fas fa-print md:text-2xl" aria-hidden="true" />
-          列印小抄
-        </button>
-        <a
-          href="https://print.ibon.com.tw/ibonprinter"
-          target="_blank"
-          class="underline text-primary my-8 lg:mt-3 lg:mb-0 block"
-          title="使用 7-11 雲端列印"
-        >
-          7-11 雲端列印
-        </a>
-        <a
-          href="https://print.famiport.com.tw/"
-          target="_blank"
-          class="underline text-primary my-8 lg:mt-3 lg:mb-0 block"
-          title="使用全家雲端列印"
-        >
-          全家雲端列印
-        </a>
+            7-11 雲端列印
+          </a>
+          <a
+            href="https://print.famiport.com.tw/"
+            target="_blank"
+            class="underline text-primary my-8 lg:mt-3 lg:mb-0 block"
+            title="使用全家雲端列印"
+          >
+            全家雲端列印
+          </a>
+        </div>
       </div>
     </div>
+    <hr class="my-3" />
+    <article>
+      <h2 class="text-xl title mb-2">編輯區使用教學</h2>
+      <p>編輯區內的所有文字都可自由編輯，包含表格的標題與內容。</p>
+      <p><i class="fa-solid fa-fill"></i> 改變表格顏色</p>
+      <p><i class="fa-solid fa-text-height"></i> 改變文字大小</p>
+      <p><i class="fa-solid fa-stopwatch"></i> 加入出發時間</p>
+      <p><i class="icon-image"></i> 為表格加入圖片</p>
+      <p><i class="fa-solid fa-plus"></i> 新增一列表格</p>
+      <p><i class="fa-solid fa-trash-alt"></i> 刪除一列表格</p>
+      <p><i>icon</i> 為該區段加上標記</p>
+    </article>
   </div>
 </template>
 
